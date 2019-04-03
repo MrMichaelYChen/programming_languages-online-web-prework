99f164ebfcf16a95b38389c5a70166a84270b0a0
Michael Chen <michaelchengaming@gmail.com>
2019-04-03 21:44:11 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,55 @@
+require"pry"
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |style, langs|
+    langs.each do |lang, type|
+      if new_hash[lang] == nil
+        new_hash[lang] = type
+        new_hash[style] = []
+      end
+        new_hash[style] << style
+        #binding.pry
+      end
+    end
+  end
+
+new_hash
+binding.pry
+
 end
+reformat_languages(languages)
